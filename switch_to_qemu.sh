459ae79dc8b4d9f36391b8f165cc32b37189c52f
Aric Gardner <agardner@linuxfoundation.com>
2014-11-19 16:30:32 -0500

    Remove expect script.

--- switch_to_qemu.sh
+++ switch_to_qemu.sh
@@ -1,4 +1,5 @@
-yum -y install libguestfs-tools
+yum install -y http://rdo.fedorapeople.org/rdo-release.rpm
+yum -y install libguestfs-tools openstack-utils 
 openstack-config --set /etc/nova/nova.conf libvirt virt_type qemu
 setsebool -P virt_use_execmem on
 ln -s /usr/libexec/qemu-kvm /usr/bin/qemu-system-x86_64
